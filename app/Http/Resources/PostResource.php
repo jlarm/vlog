4c6590952e992f2155f885c8344320b6917d58cc
jlarm <117611704+jlarm@users.noreply.github.com>
2025-09-03 14:24:02 -0500

    Added Post

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Post */
+class PostResource extends JsonResource
+{
+    public function toArray(Request $request)
+    {
+        return [
+            'id' => $this->id,
+            'uuid' => $this->uuid,
+            'title' => $this->title,
+            'body' => $this->body,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
